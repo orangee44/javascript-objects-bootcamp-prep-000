b6603ed762c7e854e6b5448bc7d172f956ca4bf8
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 16:28:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,3 +8,17 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 
+
+
+  1) objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
+
+      AssertionError: expected { Object (SaraBareilles, Slowdive, ...) } to contain keys 'Slowdive', 'My Bloody Valentine', and 'Phil Ochs'
+      + expected - actual
+
+       [
+         "My Bloody Valentine"
+      -  "SaraBareilles"
+      +  "Phil Ochs"
+         "Slowdive"
+      -  "artistName"
+       ]
